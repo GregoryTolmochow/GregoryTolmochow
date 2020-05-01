668b22fe7335ea5f27ac072483fe2639ea0bc718
GregoryTolmochow <59703138+GregoryTolmochow@users.noreply.github.com>
2020-05-01 11:37:54 -0700

    Create Gregory.py

---
+++ Gregory.py
@@ -0,0 +1,6 @@
+x = input('Enter you name:')
+print ('Welcome ' + x + ' to my game, Age Force!')
+print('If you dare to play you will be put up against the ultimate test, risking you age for success')
+y= input('Enter your age: ')
+c = input('now choose a social class: Lower class but nice, middle class and jelous, rich and stressed')
+if c == 'Lower class but nice'v
